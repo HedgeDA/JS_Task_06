f953db97e10af629af954c7e7e16adf3d88510eb
HedgeDA <faraon.da@gmail.com>
2019-01-19 01:33:55 +0600

    ...

--- feedback/js/main.js
+++ feedback/js/main.js
@@ -1,4 +1,7 @@
+let contentform;
+let output;
 let formData = {};
+let formButton;
 
 function validateString(event) {
   let span = event.target.parentElement.getElementsByTagName('p')[0].getElementsByTagName('span')[0];
@@ -11,7 +14,7 @@ function validateString(event) {
     formData[event.target.name] = event.target.value;
   }
 
-  console.log(formData);
+  formButton.disabled = (!(Object.keys(formData).length === 11));
 }
 
 function zipKeydown(event) {
@@ -19,22 +22,48 @@ function zipKeydown(event) {
     return;
   }
 
-  console.log('key ', event.key);
-  console.log('code ', event.code);
-  console.log(/^key/.test(event.code));
-  if (!/^[0-9]/.test(event.key) & /^Key/.test(event.code)) {
+  if (!/^[0-9]/.test(event.key) && /^Key/.test(event.code)) {
     event.preventDefault();
 
     return false;
   }
 }
 
-function init() {
-  for (field of document.getElementsByTagName('input')) {
-    field.addEventListener('change', validateString);
+function hideForm(event) {
+  event.preventDefault();
+
+  for (var key in formData) {
+    let element = document.getElementById(key);
+    if (element != null) {
+      element.value = formData[key];
+    }
   }
 
+  contentform.classList.add('hidden');
+  output.classList.remove('hidden');
+}
+
+function showForm(event) {
+  event.preventDefault();
+
+  output.classList.add('hidden');
+  contentform.classList.remove('hidden');
+}
+
+function init() {
+  contentform = document.querySelector('.contentform');
+  output = document.getElementById('output');
+
+  for (let field of contentform.getElementsByTagName('input')) {
+    field.addEventListener('change', validateString);
+  }
+  document.getElementsByName('message')[0].addEventListener('input', validateString);
   document.getElementsByName('zip')[0].addEventListener('keydown', zipKeydown);
+
+  formButton = contentform.getElementsByTagName('button')[0];
+  formButton.addEventListener('click', hideForm);
+
+  output.getElementsByTagName('button')[0].addEventListener('click', showForm);
 }
 
 document.addEventListener('DOMContentLoaded', init);
